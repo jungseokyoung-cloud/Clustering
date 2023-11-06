5700a0c14289410777204159376cbe01cf459f75
Jung SeokYoung <thrdud0423@gmail.com>
2023-11-06 16:49:29 +0900

    [FEAT]: KMeans 구현

--- Package.swift
+++ Package.swift
@@ -5,19 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "Clustering",
+		platforms: [.iOS(.v13)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "Clustering",
-            targets: ["Clustering"]),
+        .library(name: "Clustering", targets: ["Clustering"])
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "Clustering"),
-        .testTarget(
-            name: "ClusteringTests",
-            dependencies: ["Clustering"]),
+        .target(name: "Clustering"),
+        .testTarget(name: "ClusteringTests", dependencies: ["Clustering"])
     ]
 )
--- Sources/Clustering/Clustering.swift
+++ Sources/Clustering/Clustering.swift
@@ -1,2 +1,68 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+
+public protocol ClusteringDelegate<DataType>: AnyObject {
+	associatedtype DataType: ClusterData
+	
+	func didFinishClustering(with results: [ClusterResult<DataType>])
+}
+
+public final class Clustering<DataType: ClusterData> {
+	// MARK: - Properties
+	public weak var delegate: (any ClusteringDelegate<DataType>)?
+	
+	private let queue: OperationQueue = {
+		let queue = OperationQueue()
+		queue.underlyingQueue = .global(qos: .userInteractive)
+		queue.qualityOfService = .userInteractive
+		
+		return queue
+	}()
+	
+	// MARK: - Intializer
+	public init() { }
+}
+
+// MARK: - Run Methods
+extension Clustering {
+	/// KMeans를 실행합니다.
+	public func run(
+		data: [DataType],
+		maxIterations: Int = 20,
+		kRange: Range<Int> = (2..<14)
+	) {
+		queue.cancelAllOperations()
+		
+		let kMeansResults = kRange.map { k -> KMeans in
+			let kMeans = KMeans(k: k, data: data, maxIterations: maxIterations)
+			queue.addOperation(kMeans)
+			return kMeans
+		}
+		
+		queue.addBarrierBlock { [weak self] in
+			guard
+				let self,
+				let optimalKmeans = self.getOptimalClustering(kMeansResults)
+			else {
+				return
+			}
+			DispatchQueue.main.async {
+				let clusterResults = self.convertToClusterResults(optimalKmeans.clusters)
+				self.delegate?.didFinishClustering(with: clusterResults)
+			}
+		}
+	}
+	
+	/// Optimal한 Clustering을 리턴합니다..
+	private func getOptimalClustering(_ kMeansResults: [KMeans<DataType>]) -> KMeans<DataType>? {
+		kMeansResults.min(by: { $0.dbi < $1.dbi })
+	}
+	
+	private func convertToClusterResults(_ clusters: [Cluster<DataType>]) -> [ClusterResult<DataType>] {
+		clusters
+			.filter { !$0.group.isEmpty }
+			.map { ClusterResult(centriod: $0.centriod, group: $0.group.allValues()) }
+	}
+}
