3349f429a931b5680d0fd346ba80b1a5a7542286
Jung SeokYoung <thrdud0423@gmail.com>
2025-04-21 14:24:32 +0900

    [REFACT]: 불필요한 LinkedList 삭제

--- Sources/Clustering/Cluster.swift
+++ Sources/Clustering/Cluster.swift
@@ -13,16 +13,17 @@ final class Cluster<T: ClusterData> {
 	var centroid: Location
 		
 	/// Cluster 내부 점들
-	var group: LinkedList<T>
+	var group: [T]
 
-	private var sumOfLocation: Location
-
-	var size: Int { group.size }
-	
+	var size: Int { group.count }
+  var isEmpty: Bool { group.isEmpty }
+  
+  private var sumOfLocation: Location
+  
 	// MARK: - Initalizers
 	init(centroid: Location) {
 		self.centroid = centroid
-		self.group = LinkedList<T>()
+		self.group = [T]()
 		sumOfLocation = .zero
 	}
 }
@@ -37,17 +38,16 @@ extension Cluster {
 	@discardableResult
 	/// 해당 값을 가지는 점을 Cluster에서 제거합니다.
 	func remove(_ data: T) -> T? {
-		let index = group.indexOf(data)
-		
-		guard let value = group.remove(at: index) else { return nil }
-		
+    guard let index = group.firstIndex(where: { $0 == data }) else { return nil }
+
+    let value = group.remove(at: index)
 		sumOfLocation -= data.location
 		return value
 	}
 	
 	/// 다른 Cluster와 합칩니다.
 	func combine(with other: Cluster) {
-		self.group.merge(other: other.group)
+    self.group += other.group
 		sumOfLocation += other.sumOfLocation
 		updateCentroid()
 	}
@@ -57,8 +57,8 @@ extension Cluster {
 extension Cluster {
 	/// Cluster내의 모든 점의 평균을 통해 Centriod를 업데이트합니다.
 	func updateCentroid() {
-		if group.size == 0 { return }
-		centroid = sumOfLocation / Double(group.size)
+    guard size != 0 else { return }
+		centroid = sumOfLocation / Double(size)
 	}
 }
 
@@ -67,9 +67,9 @@ extension Cluster {
 	/// Cluster의 Centriod로부터 분산을 리턴합니다.
 	func deviation(from data: T) -> Double {
 		// 해당 데이터가 클러스터내에 위치한다면, -1을 해줍니다.
-		let divisor = group.contain(data) ? group.size - 1 : group.size
-		
-		return group.allValues()
+    let divisor = group.contains(data) ? group.count - 1 : group.count
+
+		return group
 			.map { $0.location }
 			.reduce(0) { $0 + data.location.distance(with: $1) } / Double(divisor)
 	}
